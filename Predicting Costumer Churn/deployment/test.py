d808a5bee8ca09f1ce3e106e5228abdec910b426
Yefry Lopez <yefry.lopez.nu@gmail.com>
2021-03-25 21:34:58 -0600

    Add project and dataset

---
+++ Predicting Costumer Churn/deployment/test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 20:18:47 2021
+
+@author: Yefry Lopez
+"""
+
+
+from flask import Flask
+
+app = Flask('test')
+
+
+@app.route('/ping', methods=['GET'])
+def ping():    
+    return 'PONG'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
